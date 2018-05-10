76f91ee47a846073aa0717257b4c5140a705e6cb
alexander <alexfrx94@gmail.com>
2018-05-10 13:58:44 -0500

    Iniciar Proyecto 6

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const app = express()
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000
 
-let PGerminal = [
-    {periodo:"semana 1", estado: "El embrion no tiene caracteristicas distintivas ", },  
+let Etapas= [
+    {periodo:"Etapa 1", estado: "El embrion no tiene caracteristicas distintivas ", },  
+	{periodo:"Etapa 2", estado: "El embrion pasa a llamarse feto ", },
+	{periodo:"Etapa 3", estado: "En una ecografia se puede detectar los brazos, piernas y sexo de la criatura", },  
+	{periodo:"Etapa 4", estado: "El feto esta completamente desarrollado y listo para nacer", },  
 ];
 
 app.use(bodyParser.json());
@@ -18,8 +21,8 @@ app.get('/', (req, res) => {
     res.status(200).send("Welcome to API REST")
 })
 
-app.get('/PGerminal', (req, res) => {
-    res.send(PGerminal)
+app.get('/Etapas', (req, res) => {
+    res.send(Etapas)
 })
 
 http.createServer(app).listen(PORT, () => {
