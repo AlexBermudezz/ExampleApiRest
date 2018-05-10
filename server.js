af0ae340277ec60dca59d5700504269b24cced99
alexander <alexfrx94@gmail.com>
2018-05-10 13:49:25 -0500

    Iniciar Proyecto 5

--- server.js
+++ server.js
@@ -6,63 +6,22 @@ const app = express()
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000
 
-let users = ['oscar', 'juan', 'marcos', 'julieta', 'alexander',];
-let books = [ [Periodo Germinal
-    {titulo: 'El señor de los anillos', autor: 'J.R.R. Tolkien'},
-    {titulo: "Cancion de hielo y fuego", autor: 'George RR Martin'}
-]];
+let PGerminal = [
+    {periodo:"semana 1",estado: "El embrion no tiene caracteristicas distintivas " },  
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ********************************************************************
-// ********************************************************************
 
-// URL raiz de la api
-// http://127.0.0.1:5000
 app.get('/', (req, res) => {
     res.status(200).send("Welcome to API REST")
 })
 
-// URL para listar todos los usuarios
-// http://127.0.0.1:5000/users
-app.get('/users', (req, res) => {
-    res.send(users)
-})
-
-app.get('/books', (req, res) => {
+app.get('/PGerminal', (req, res) => {
     res.send(books)
 })
 
-// URL para eliminar un usuario
-// http://127.0.0.1:5000/users
-app.post('/users', (req, res) => {
-    let data = req.query;
-    users.push(data.user_name)
-    res.send("New user add")
-})
-
-// URL para actualizar un usuario
-// http://127.0.0.1:5000/users/1
-app.patch('/users/:id',(req, res) => {
-    let params = req.params;
-    let data = req.query;
-    users[params.id] = data.user_name
-    res.send("User update")
-})
-
-// URL para eliminar un usuario
-// http://127.0.0.1:5000/users/1
-app.delete('/users/:id',(req, res) => {
-    let params = req.params;
-    users.splice(params.id, 1);
-    res.send('User delete')
-})
-
-// ********************************************************************
-// ********************************************************************
-
-// Crear y lanzar el servidor
 http.createServer(app).listen(PORT, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
 })
